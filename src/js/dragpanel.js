682089c60f534f5826b154cb1b0cbfb5b011fb53
zhongxiaodong <zxdzyadmin@yonyou.com>
2018-06-05 17:47:56 +0800

    修改

--- src/js/dragpanel.js
+++ src/js/dragpanel.js
@@ -34,8 +34,9 @@
       //'<div class="pp-modal-wrap" >'
       //'<div class="pp-modal" id="ppmodal" style="width: 500px; transform-origin: -30px 431px 0px;">'
       '<div class="pp-modal-content" id="targetEle">'
+      + '<button aria-label="Close" class="pp-modal-close" ><span class="pp-modal-close-x">-</span></button>'
       + '<button aria-label="Close" class="pp-modal-close" id="closebtn"><span class="pp-modal-close-x">X</span></button>'
-      + '<div class="pp-modal-header" id="dragEle"><div class="pp-modal-title" id="rcDialogTitle2">图片预览</div></div>'
+      + '<div class="pp-modal-header" id="dragEle"><div class="pp-modal-title" id="rcDialogTitle2">弹框拖动插件</div></div>'
       + '<div class="pp-modal-body">'
       + '<div class="pp-scale">'
       + '<div class="pp-card pp-card-bordered" id="normalimgdiv">'
@@ -60,8 +61,8 @@
   DragPanel.prototype.close = function () {
     var closebtn = document.getElementById('closebtn');
     closebtn.onclick = function (e) {
-      var drag = document.getElementById('drag');
-      drag.remove();
+      var drag = document.getElementById('targetEle');
+      targetEle.remove();
     }
   };
 
@@ -72,54 +73,7 @@
   DragPanel.prototype.drag = function () {
     var targetEle = document.getElementById('targetEle');
     var dragEle = document.getElementById('dragEle');
-    var dragCallback = function (left, top) {
-      console.log(left);
-      console.log(top);
-    };
-    startDrag(targetEle, dragEle, false, dragCallback());
 
-    
-    /*drag.onmousedown = function (e) {
-      var e = e || window.event;
-      debugger;
-      var diffX = e.clientX - drag.offsetLeft;
-      var diffY = e.clientY - drag.offsetTop;
-      if (typeof drag.setCapture != 'undefined') {
-        drag.setCapture();
-      }
-      drag.onmousemove = function (e) {
-        var e = e || window.event; //兼容ie浏览器  
-        var left = e.clientX - diffX;
-        var top = e.clientY - diffY;
-
-        //控制拖拽物体的范围只能在浏览器视窗内，不允许出现滚动条  
-        if (left < 0) {
-          left = 0;
-        } else if (left > window.innerWidth - drag.offsetWidth) {
-          left = window.innerWidth - drag.offsetWidth;
-        }
-        if (top < 0) {
-          top = 0;
-        } else if (top > window.innerHeight - drag.offsetHeight) {
-          top = window.innerHeight - drag.offsetHeight;
-        }
-        //移动时重新得到物体的距离，解决拖动时出现晃动的现象  
-        drag.style.left = left + 'px';
-        drag.style.top = top + 'px';
-      };
-
-      drag.onmouseup = function (e) { //当鼠标弹起来的时候不再移动  
-        this.onmousemove = null;
-        this.onmouseup = null; //预防鼠标弹起来后还会循环（即预防鼠标放上去的时候还会移动）  
-        //修复低版本ie bug  
-        if (typeof drag.releaseCapture != 'undefined') {
-          drag.releaseCapture();
-        }
-      };
-    }; */
-  };
-
-  var startDrag = function (targetEle, dragEle, inWindow, callback) {
     var dragParams = {
       left: 0,
       top: 0,
@@ -127,7 +81,7 @@
       currentY: 0
     };
 
-    bindEventFunc(dragEle, "onmousedown", function (e) {
+    dragEle.onmousedown = function (e) {
       e.preventDefault();
       var e = e ? e : window.event;
       //1、获取拖拽窗口的偏移量
@@ -136,22 +90,34 @@
       //2、获取当前鼠标坐标点
       dragParams.currentX = e.clientX;
       dragParams.currentY = e.clientY;
-    });
+      document.body.onmousemove = function (e) {
+        e.preventDefault();
+        var e = e ? e : window.event;
+        console.log('鼠标坐标：'+ e.clientX + '---' + e.clientY);
+        var nowX = e.clientX;
+        var nowY = e.clientY;
+        var diffX = nowX - dragParams.currentX;
+        var diffY = nowY - dragParams.currentY; 
+        var maxTop = document.body.offsetHeight - targetEle.offsetHeight,
+        maxLeft = document.body.offsetWidth - targetEle.offsetWidth;
+        if (dragParams.top < 0) dragParams.top = 0;
+        if (dragParams.top > maxTop) top = maxTop;
+        if (dragParams.left < 0) dragParams.left = 0;
+        if (dragParams.left > maxLeft) dragParams.left = maxLeft;
+        var left = parseInt(diffX + dragParams.left);
+        var top = parseInt(diffY + dragParams.top);
+        targetEle.style.left = left + "px";
+        targetEle.style.top = top + "px";
+      };
 
-    bindEventFunc(document.body,"onmouseup",function(e){
-      e.preventDefault();
-      var e = e ? e : window.event;
-      var nowX = e.clientX;
-      var nowY = e.clientY;
-      var diffX = nowX - dragParams.currentX;
-      var diffY = nowY - dragParams.currentY;
-      var left = parseInt(diffX + dragParams.left);
-      var top = parseInt(diffY + dragParams.top);
-      targetEle.style.left = left + "px";
-      targetEle.style.top = top + "px";
-    });
+      document.body.onmouseup = function (e) {
+        document.body.onmousemove = null;
+        document.body.onmouseup = null;
+      };
+    };
 
   };
+
 
   /**
    * 添加绑定事件函数，兼容智障IE浏览器
